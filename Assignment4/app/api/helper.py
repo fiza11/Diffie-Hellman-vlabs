d46a6fb40f466d271dbb523224e057259a062502
Fiza Husain <43874107+fiza11@users.noreply.github.com>
2019-04-19 16:32:47 +0530

    Update helper.py

--- Assignment4/app/api/helper.py
+++ Assignment4/app/api/helper.py
@@ -59,8 +59,9 @@ def generate_prime_number(length=128):
 def genp(l):
     return generate_prime_number(int(l))
 
-def geng(l):
-    return generate_prime_number(int(l-1))
+def geng(p):
+    binary = bin(int(p))[2:]
+    return generate_prime_number(len(binary)-1)
 
 def genpk(p):
     return randrange(1,int(p))
@@ -75,43 +76,33 @@ except:
     p = generate_prime_number(l)
 print("Prime: " + str(p))
 print('')
-
-
 try:
     g = input("Enter a generator (leave blank to auto generate): ")
 except: 
     g=generate_prime_number(l-1)
 print("Generator: "+ str(g))
 print('')
-
 try:
     a = input("Enter Alice's Private key (leave blank to auto generate): ")
 except: 
     a=randrange(1,p)
 print("Alice's private key: " + str(a))
 print('')
-
 try:
     b = input("Enter Bob's Private key (leave blank to auto generate): ")
 except: 
     b=randrange(1,p)
 print("Bob's private key: " + str(b))
 print('')
-
 Ga = pow(int(g),int(a),int(p))
 Gb = pow(int(g),int(b),int(p))
-
 print("Ga: " + str(Ga))
 print("Gb: " + str(Gb))
 print('')
-
 Gab = pow(int(Gb),int(a),int(p))
 Gba = pow(int(Ga),int(b),int(p))
-
 print("Alice's secret key (Gab): " + str(Gab))
 print("Bob's secret key (Gba): " + str(Gba))
 print('')
-
 print("As both of them match, a Diffie Hellman Key was successfully established!")
 """
-
